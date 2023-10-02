ff0bf95ca483aee1dfd8b83eec94a1d32c0e5a7e
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-10-02 17:08:31 -0400

    spring app hoppers receipt practice

---
+++ src/main/java/com/codingdojo/springhoppersreceipt/SpringHoppersReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.springhoppersreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHoppersReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHoppersReceiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/springhoppersreceipt/SpringHoppersReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.springhoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHoppersReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
